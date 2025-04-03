7298a03e1353b66224bafdddfda0858a15e2d9d5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-04-03 22:39:38 +0900

    fix: tailwind config 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     screens: {
       sm: { min: "360px", max: "959px" },
