388f1a79efeb7b8fc6b74d045fa78ff18721809d
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-04-03 21:16:04 +0900

    fix: tailwind.config 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,11 @@ module.exports = {
       lg: { min: "1920px" },
     },
     extend: {
-      color: {
+      colors: {
         primary: "#F22020",
         point: "#FF8E8E",
         gray: {
-          0: "#222222",
+          "0": "#222222",
           100: "#333333",
           200: "#555555",
           300: "#999999",
@@ -37,47 +37,17 @@ module.exports = {
         bgColor: "#161616",
         bgContent: "#1A1A1A",
       },
-      textStyle: {
-        xxs: {
-          fontSize: "10px",
-          lineHeight: 1.5,
-        },
-        xs: {
-          fontSize: "12px",
-          lineHeight: 1.5,
-        },
-        s: {
-          fontSize: "13px",
-          lineHeight: 1.5,
-        },
-        sm: {
-          fontSize: "14px",
-          lineHeight: 1.5,
-        },
-        base: {
-          fontSize: "16px",
-          lineHeight: 1.5,
-        },
-        lg: {
-          fontSize: "18px",
-          lineHeight: 1.5,
-        },
-        xl: {
-          fontSize: "20px",
-          lineHeight: 1.4,
-        },
-        "2xl": {
-          fontSize: "24px",
-          lineHeight: 1.4,
-        },
-        "3xl": {
-          fontSize: "32px",
-          lineHeight: 1.3,
-        },
-        "4xl": {
-          fontSize: "40px",
-          lineHeight: 1.3,
-        },
+      fontSize: {
+        xxs: ["10px", { lineHeight: "1.5" }],
+        xs: ["12px", { lineHeight: "1.5" }],
+        s: ["13px", { lineHeight: "1.5" }],
+        sm: ["14px", { lineHeight: "1.5" }],
+        base: ["16px", { lineHeight: "1.5" }],
+        lg: ["18px", { lineHeight: "1.5" }],
+        xl: ["20px", { lineHeight: "1.4" }],
+        "2xl": ["24px", { lineHeight: "1.4" }],
+        "3xl": ["32px", { lineHeight: "1.3" }],
+        "4xl": ["40px", { lineHeight: "1.3" }],
       },
       fontWeight: {
         light: "300",
