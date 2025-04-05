c1360ccc548668999f719a0e03c2fb78abff6e5e
chaeyun-sim <111689342+chaeyun-sim@users.noreply.github.com>
2025-04-05 12:44:28 +0900

    fix: tailwind config 수정, 공통 컴포넌트 구현 (#4)
    * chore: 라이브러리 초기 세팅  (#2)
    
    * chroe: zod 및 react-hook-form 설치
    
    * chore: axios 설치
    
    * chore: zustand 설치
    
    * chore: tanstack query 설치
    
    * chore: 탄스택 쿼리 초기 세팅
    
    * chore: tailwind config 생성 및 스타일 작성 (#3)
    
    * feat: viewport meta 태그 추가
    
    * chore: tailwind.config.js 생성 및 screens 작성
    
    * chore: color, textStyle, fontWeight config 추가
    
    * fix: tailwind.config 수정
    
    * fix: tailwind config 수정
    
    * fix: tailwind config version에 맞게 수정
    
    * fix: global css 수정
    
    * chore: shadcn 설치
    
    * feat: 버튼 컴포넌트 구현
    
    * feat: 체크박스 컴포넌트 구현
    
    * feat: 라디오 컴포넌트 구현
    
    * feat: 스위치 컴포넌트 구현
    
    * feat: 슬라이더 컴포넌트 구현
    
    * remove: 불필요한 파일 삭제
    
    * fix: important 제거
    
    ---------
    
    Co-authored-by: ssseeo0 <135966211+ssseeo0@users.noreply.github.com>

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,64 @@
+import cn from "@/lib/utils";
+import { Slot } from "@radix-ui/react-slot";
+import { type VariantProps, cva } from "class-variance-authority";
+import * as React from "react";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none cursor-pointer",
+  {
+    variants: {
+      variant: {
+        outline: "border",
+        default: "",
+      },
+      color: {
+        primary: "bg-primary !text-white hover:bg-point",
+        black: "bg-gray-0 !text-white",
+        grey: "bg-gray-700 !text-gray-300",
+        "outline-primary": "border-primary !text-primary",
+        "outline-black": "border-gray-200 !text-gray-200",
+        "outline-gray": "border-gray-500 !text-gray-200",
+      },
+      size: {
+        sm: "px-[16px] h-[36px] rounded-[8px] text-s font-medium",
+        md: "px-[24px] h-[44px] rounded-[8px] text-sm font-medium",
+        lg: "px-[24px] h-[48px] rounded-[12px] text-[15px] font-semibold",
+        xl: "px-[32px] h-[56px] rounded-[12px] text-base font-semibold",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      color: "primary",
+      size: "sm",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+  color:
+    | "primary"
+    | "black"
+    | "grey"
+    | "outline-primary"
+    | "outline-black"
+    | "outline-gray";
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, color, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, color, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
