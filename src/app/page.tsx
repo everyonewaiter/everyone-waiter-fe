8f2f8af85b04a8f83a5183b3359dbe643543bff5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-04-06 22:33:20 +0900

    feat: 개인 정보 저장 store 구현 및 API 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,47 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { getAccount } from "@/lib/api/auth.api";
+import { useQuery } from "@tanstack/react-query";
+import { useAccount } from "@/hooks/store/useAccount";
 import Splash from "./(splash)/page";
 
 export default function Home() {
   const FADE_OUT_DURATION = 1000;
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [fadeOut, setFadeOut] = useState(false);
 
+  const { setProfile, setIsLoggedIn } = useAccount();
+
+  const { data: profile } = useQuery({
+    queryKey: ["my"],
+    queryFn: getAccount,
+  });
+
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setFadeOut(true);
-      setTimeout(() => setIsLoading(false), FADE_OUT_DURATION);
-    }, 2000);
-    return () => clearTimeout(timer);
+    if (profile?.accountId!) {
+      setProfile(profile);
+      setIsLoggedIn(true);
+    }
+  }, [profile]);
+
+  useEffect(() => {
+    const hideSplash = localStorage.getItem("hideSplash");
+
+    if (hideSplash === "false" || !hideSplash) {
+      setIsLoading(true);
+      const timer = setTimeout(() => {
+        setFadeOut(true);
+        setTimeout(() => {
+          localStorage.setItem("hideSplash", "true");
+          setIsLoading(false);
+        }, FADE_OUT_DURATION);
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
+
+    return () => {};
   }, []);
 
   return (
