1f5b7434457cb543952d831bbbbc75a509daa111
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-04-06 12:28:22 +0900

    feat: 스플래시 구현

---
+++ src/app/(splash)/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Splash({
+  fadeOut,
+  duration,
+}: {
+  fadeOut: boolean;
+  duration: number;
+}) {
+  return (
+    <div
+      className={`bg-primary ${fadeOut ? "opacity-0" : "opacity-100"} hidden h-screen w-screen items-center justify-center duration-${duration} sm:flex`}
+    >
+      <Image
+        src="/images/logo-with-text-white.svg"
+        alt="splash"
+        width={160}
+        height={97}
+      />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Splash from "./(splash)/page";
+
 export default function Home() {
-  return <div>hi</div>;
+  const FADE_OUT_DURATION = 1000;
+  const [isLoading, setIsLoading] = useState(true);
+  const [fadeOut, setFadeOut] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFadeOut(true);
+      setTimeout(() => setIsLoading(false), FADE_OUT_DURATION);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      {isLoading && <Splash fadeOut={fadeOut} duration={FADE_OUT_DURATION} />}
+      <div>hi</div>
+    </>
+  );
 }
