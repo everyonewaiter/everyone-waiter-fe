925d1b6221730118a5667667178238c0ab8b6d03
seo02wow <rlatjdud9188@naver.com>
2025-04-07 00:13:26 +0900

    design: 캘린더 디자인 커스텀 (기간 수정 필요)

--- src/components/common/Calender.tsx
+++ src/components/common/Calender.tsx
@@ -26,7 +26,7 @@ function Calendar({
     <DayPicker
       showOutsideDays={showOutsideDays}
       className={cn(
-        "flex min-w-[550px] items-center justify-center bg-white p-6",
+        "flex min-w-[550px] items-center justify-center rounded-3xl bg-white p-6 shadow-xs",
         className
       )}
       formatters={{
@@ -36,14 +36,14 @@ function Calendar({
       classNames={{
         // 월 컨테이너
         months: "flex flex-col sm:flex-row gap-2",
-        month: "flex flex-col gap-4",
+        month: "flex flex-col gap-8",
         // 상단 네비게이션 영역
         caption: "flex justify-center pt-1 relative items-center w-full",
         caption_label: "text-xl font-bold text-gray-0",
         nav: "flex items-center gap-1",
         nav_button: "size-7 bg-transparent p-0 opacity-50 hover:opacity-100",
-        nav_button_previous: "absolute left-40 text-gray-200",
-        nav_button_next: "absolute right-40 text-gray-200",
+        nav_button_previous: "absolute left-36 text-gray-200",
+        nav_button_next: "absolute right-32 text-gray-200",
         // 달력 테이블
         table: "w-full border-collapse space-x-1",
         head_row: "flex items-center gap-[46px]",
@@ -58,7 +58,7 @@ function Calendar({
             : "[&:has([aria-selected])]:rounded-md"
         ),
         day: cn(
-          "hover:cursor-pointer disabled:cursor-not-allowed hover:bg-primary/[0.06] rounded-full size-10 p-0 font-normal aria-selected:text-white aria-selected:hover:bg-primary  text-gray-0 text-sm"
+          "hover:cursor-pointer disabled:cursor-not-allowed hover:bg-primary/[0.06] rounded-full size-10 p-0 font-normal aria-selected:text-white text-gray-0 text-sm"
         ),
         // 특수 상태의 날짜
         // 선택된 날짜
@@ -67,16 +67,14 @@ function Calendar({
         // 오늘 날짜
         day_today: "bg-accent text-accent-foreground",
         // 현재 월에 속하지 않는 날짜
-        day_outside: "text-[#C0C0C0]",
+        day_outside: "text-gray-800",
         // 비활성화된 날짜
         day_disabled: "text-muted-foreground opacity-50",
         // 범위 선택 시 스타일
         day_range_start:
-          "day-range-start aria-selected:bg-primary rounded-full aria-selected:text-primary-foreground",
-        day_range_end:
-          "day-range-end aria-selected:bg-primary rounded-full aria-selected:text-primary-foreground",
-        day_range_middle:
-          "aria-selected:bg-accent aria-selected:text-accent-foreground",
+          "day-range-start aria-selected:bg-primary rounded-full ",
+        day_range_end: "day-range-end aria-selected:bg-primary rounded-full ",
+        day_range_middle: "aria-selected:bg-primary/[0.06]",
         // 숨겨진 날짜
         day_hidden: "invisible",
         ...classNames,
