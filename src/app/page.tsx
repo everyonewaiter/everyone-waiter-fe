2bc27e12f49e1fd636fb8faf5e9170b197f0049b
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-04-07 12:52:51 +0900

    fix: 토큰 관련 useEffect 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,29 @@ import { useEffect, useState } from "react";
 import { getAccount } from "@/lib/api/auth.api";
 import { useQuery } from "@tanstack/react-query";
 import { useAccount } from "@/hooks/store/useAccount";
+import { getToken } from "@/lib/cookies";
 import Splash from "./(splash)/page";
 
 export default function Home() {
   const FADE_OUT_DURATION = 1000;
   const [isLoading, setIsLoading] = useState(false);
   const [fadeOut, setFadeOut] = useState(false);
+  const [token, setToken] = useState("");
 
   const { setProfile, setIsLoggedIn } = useAccount();
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      const accessToken = await getToken("accessToken");
+      setToken(accessToken!);
+    };
+    fetchToken();
+  }, []);
 
   const { data: profile } = useQuery({
     queryKey: ["my"],
     queryFn: getAccount,
+    enabled: !!token,
   });
 
   useEffect(() => {
