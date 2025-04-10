d0065f65d6a2fb7ebf719125d05bad5f61ddb8f0
ssseeo0 <135966211+ssseeo0@users.noreply.github.com>
2025-04-11 00:31:53 +0900

    feat: 레이아웃 구현 (#13)
    * feat: 모바일 헤더 및 사이드바 모달 구현
    
    * design: 사이드바 디자인 적용
    
    * design: 사이드바 디자인 수정
    
    * design: 반응형 레이아웃 구현

---
+++ src/app/(main)/_components/MobileHeader.tsx
@@ -0,0 +1,19 @@
+import SideBarButton from "@/app/(main)/_components/SideBarButton";
+import Image from "next/image";
+
+export default function MobileHeader() {
+  return (
+    <header className="flex h-[60px] items-center border-b border-gray-600 bg-white px-5 md:hidden">
+      <SideBarButton />
+      <div className="flex w-full items-center justify-center gap-3">
+        <Image
+          src="/logo.svg"
+          alt="모두의 웨이터 로고"
+          width={24}
+          height={24}
+        />
+        <h1 className="font-hakgyo text-primary text-lg">모두의 웨이터</h1>
+      </div>
+    </header>
+  );
+}
---
+++ src/app/(main)/_components/SideBarButton.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import MobileSidebar from "@/app/(main)/_components/MobileSidebar";
+import useOverlay from "@/hooks/use-overlay";
+import { Menu } from "lucide-react";
+
+export default function SideBarButton() {
+  const { open, close } = useOverlay();
+
+  const handleOpenSidebar = () => {
+    open(() => <MobileSidebar onClose={close} />);
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={handleOpenSidebar}
+      className="absolute left-5 cursor-pointer"
+    >
+      <Menu color="#222" width={24} height={24} />
+    </button>
+  );
+}
