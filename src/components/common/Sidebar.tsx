9e4d3e982476fa6e6749fa5ce4b71bde4c1e5055
seo02wow <rlatjdud9188@naver.com>
2025-04-10 23:51:15 +0900

    design: 사이드바 디자인 수정

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -20,23 +20,25 @@ export default function Sidebar({ onClose }: SideBarProps) {
     <div className="bg-opacity-100 fixed inset-0 z-[9999] flex backdrop-blur-sm md:hidden">
       <aside
         ref={ref}
-        className="relative flex h-screen w-[312px] flex-col overflow-auto rounded-tr-[10px] rounded-br-[10px] bg-white px-4 py-[28px]"
+        className="relative flex h-screen w-[312px] flex-col overflow-auto rounded-tr-[10px] rounded-br-[10px] bg-white px-4 py-5"
       >
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-2">
-            <div className="flex size-9 items-center justify-center rounded-2xl bg-[#F4F4F4]">
-              <Image
-                src="/images/logo-cloche.svg"
-                alt="모두의 웨이터 로고"
-                width={26}
-                height={14}
-              />
-            </div>
-            <h2 className="text-primary text-xl font-semibold">
+        <div className="flex items-center justify-between border-b border-gray-600 pb-4">
+          <div className="flex items-center gap-3">
+            <Image
+              src="/logo.svg"
+              alt="모두의 웨이터 로고"
+              width={40}
+              height={40}
+            />
+            <h2 className="text-primary font-hakgyo text-xl font-semibold">
               모두의 웨이터
             </h2>
           </div>
-          <button type="button" className="absolute top-[34px] left-[272px]">
+          <button
+            type="button"
+            className="absolute top-[29px] left-[272px] cursor-pointer"
+            onClick={onClose}
+          >
             <CloseIcon color="#222" width={24} height={24} />
           </button>
         </div>
