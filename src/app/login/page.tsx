4b3ab4ccd4cc17edfe7af3c5a412c82e81d3fa81
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-04-16 23:16:09 +0900

    style: 레이아웃 관련 아이콘 생성

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -44,7 +44,7 @@ export default function Login() {
   return (
     <SignupLayout>
       <Image
-        src="/images/logo-with-text.svg"
+        src="/icons/logo/logo-with-text.svg"
         alt="logo with text"
         className="h-[97px] w-[160px] md:h-[94px] md:w-[154px] lg:h-[124px] lg:w-[200px]"
         width={200}
