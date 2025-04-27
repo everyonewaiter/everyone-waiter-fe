06b7368618f9350c2e5133a0a8ad348152a04c81
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-04-27 13:33:04 +0900

    feat: useDebounce 구현

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+interface IProps {
+  searchWord: string;
+  delay: number;
+}
+
+export default function useDebounce({ searchWord, delay }: IProps) {
+  const [debouncedValue, setDebouncedValue] = useState(searchWord);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(searchWord);
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchWord]);
+
+  return {
+    debouncedValue,
+  };
+}
