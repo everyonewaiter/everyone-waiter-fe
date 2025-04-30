f52c5d99c383243e054eca32b648d1c0df8bb9c7
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-04-30 10:45:28 +0900

    fix: 테이블 수정

--- src/app/admin/users/_components/UsersTableRow.tsx
+++ src/app/admin/users/_components/UsersTableRow.tsx
@@ -1,47 +1,34 @@
-import QueryProviders from "@/app/query-providers";
 import Checkbox from "@/components/common/Checkbox";
 import ResponsiveButton from "@/components/common/ResponsiveButton";
 import { TableCell, TableRow } from "@/components/common/Table/Tables";
-import { PermissionObj } from "@/constants/permissionObj";
-import useOverlay from "@/hooks/use-overlay";
+import { PermissionObj, stateObj } from "@/constants/permissionObj";
 import cn from "@/lib/utils";
-import UserInfoModal from "../../_components/UserInfoModal";
 
-export default function UsersTableRow({ ...item }: AdminAccount) {
-  const { open, close } = useOverlay();
+interface IProps extends AdminAccount {
+  openModal: () => void;
+}
 
-  const handleOpenModal = (accountId: bigint) => {
-    open(() => (
-      <QueryProviders>
-        <UserInfoModal close={close} accountId={accountId} />
-      </QueryProviders>
-    ));
-  };
-
+export default function UsersTableRow({ openModal, ...item }: IProps) {
   return (
-    <TableRow onClick={() => handleOpenModal(item.accountId)}>
-      <TableCell className="w-[68px]">
+    <TableRow onClick={openModal}>
+      <TableCell className="md:w-[64px] lg:w-[68px]">
         <Checkbox />
       </TableCell>
-      <TableCell className="flex-[1.3]">{item.email}</TableCell>
-      <TableCell className="flex-1">{item.createdAt}</TableCell>
+      <TableCell className="flex-[1.3]">
+        <span className="hidden lg:block">{item.email}</span>
+        <span className="hidden md:block lg:hidden">
+          {item.email.length > 15
+            ? `${item.email.slice(0, 15)}...`
+            : item.email}
+        </span>
+      </TableCell>
+      <TableCell className="flex-1">{item.createdAt.slice(2, 16)}</TableCell>
       <TableCell className="flex flex-1 justify-center">
         <ResponsiveButton
           responsiveButtons={{
             lg: {
               buttonSize: "md",
-              className: cn(
-                "!h-[37px] !px-5 !py-2 !rounded-[24px]",
-                item.permission === "OWNER"
-                  ? "!bg-[#3900B508] !text-[#3900B5]"
-                  : "",
-                item.permission === "USER"
-                  ? "!bg-[#00C03010] !text-[#00C030]"
-                  : "",
-                item.permission === "ADMIN"
-                  ? "!bg-[#FFB70008] !text-[#FFB700]"
-                  : ""
-              ),
+              className: "!h-[37px] !px-5 !py-2 !rounded-[24px]",
             },
             md: {
               buttonSize: "md",
@@ -49,15 +36,20 @@ export default function UsersTableRow({ ...item }: AdminAccount) {
                 "!h-[26px] !rounded-[24px] !bg-[#3900B508] !text-[#3900B5] !text-xs !font-regular !px-3 !py-1",
             },
           }}
+          commonClassName={cn(
+            item.permission === "OWNER"
+              ? "!bg-[#3900B508] !text-[#3900B5]"
+              : "",
+            item.permission === "USER" ? "!bg-[#00C03010] !text-[#00C030]" : "",
+            item.permission === "ADMIN" ? "!bg-[#FFB70008] !text-[#FFB700]" : ""
+          )}
         >
           {PermissionObj[item.permission as TPermission]}
         </ResponsiveButton>
       </TableCell>
-      <TableCell className="flex-1">??</TableCell>
-      <TableCell className="flex-1">
-        {item.hasStore === "Y" ? "구독중" : "-"}
-      </TableCell>
-      <TableCell className="flex-1">{item.stats || "-"}</TableCell>
+      <TableCell className="flex-1">-</TableCell>
+      <TableCell className="flex-1">{item.hasStore}</TableCell>
+      <TableCell className="flex-1">{stateObj[item.state] || "-"}</TableCell>
     </TableRow>
   );
 }
--- src/components/common/Table/Tables.tsx
+++ src/components/common/Table/Tables.tsx
@@ -79,7 +79,7 @@ const TableCell = React.forwardRef<
   <td
     ref={ref}
     className={cn(
-      "text-gray-0 text-s font-regular py-4 text-center lg:text-base",
+      "text-gray-0 font-regular py-4 text-center text-xs lg:text-base",
       className
     )}
     {...props}
@@ -140,7 +140,7 @@ const MobileTableCell = React.forwardRef<
   <td
     ref={ref}
     className={cn(
-      "!text-s text-gray-0 flex w-[180px] items-center justify-center border-b border-b-gray-600 px-6 text-center font-medium",
+      "!text-s text-gray-0 font-regular flex w-[180px] items-center justify-center border-b border-b-gray-600 px-6 text-center",
       className
     )}
     {...props}
