a0b705961deb58d68c6c53f7eb97c1a642e1c3bb
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-05-31 20:48:25 +0900

    fix: /control 삭제

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Image from "next/image";
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import InfoPopup from "../InfoPopup";
 
 interface IProps {
@@ -11,7 +11,6 @@ interface IProps {
 
 export default function Header({ openMobileSidebar }: IProps) {
   const navigate = useRouter();
-  const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -21,9 +20,6 @@ export default function Header({ openMobileSidebar }: IProps) {
           <button
             type="button"
             className="flex items-center md:gap-[13px] lg:gap-5"
-            onClick={() =>
-              pathname.startsWith("/control") ? null : navigate.push("/")
-            }
           >
             <Image
               src="/icons/logo/logo-medium.svg"
@@ -40,22 +36,20 @@ export default function Header({ openMobileSidebar }: IProps) {
               className="md:h-4 md:w-[94px] lg:h-[25px] lg:w-[141px]"
             />
           </button>
-          {!pathname.startsWith("/control") && (
-            <button
-              type="button"
-              className="center relative h-8 w-8 rounded-[12px] border border-gray-400 lg:h-12 lg:w-12 lg:rounded-[16px]"
-              onClick={() => setIsOpen((prev) => !prev)}
-            >
-              <Image
-                src="/icons/user.svg"
-                alt="사이드 메뉴"
-                width={24}
-                height={24}
-                className="md:h-6 md:w-6 lg:h-8 lg:w-8"
-              />
-              {isOpen && <InfoPopup close={() => setIsOpen(false)} />}
-            </button>
-          )}
+          <button
+            type="button"
+            className="center relative h-8 w-8 rounded-[12px] border border-gray-400 lg:h-12 lg:w-12 lg:rounded-[16px]"
+            onClick={() => setIsOpen((prev) => !prev)}
+          >
+            <Image
+              src="/icons/user.svg"
+              alt="사이드 메뉴"
+              width={24}
+              height={24}
+              className="md:h-6 md:w-6 lg:h-8 lg:w-8"
+            />
+            {isOpen && <InfoPopup close={() => setIsOpen(false)} />}
+          </button>
         </div>
         <div className="h-[1px] bg-gray-500" />
       </div>
