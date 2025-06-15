eb70adbdefbd866532641e46e70fc1d5b0a2b26d
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-06-15 10:59:24 +0900

    style: gap 작성 타입 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
               title="매장 등록 승인을 기다리고 있습니다."
               subtitle="관리자의 승인이 완료될 때까지 1~2일 소요될\n예정이니 양해 부탁드립니다."
               image={{ url: "/gif/hourglass.gif", size: 160 }}
-              gap={5}
+              gap="gap-[5px]"
               href="/stores"
             />
           )}
@@ -49,7 +49,7 @@ export default function Home() {
               title="매장 등록 신청이 반려되었습니다."
               subtitle="반려 사유 관련 메일을 발송했습니다.\n메일함을 확인해주세요."
               image={{ url: "/gif/rejected.gif", size: 160 }}
-              gap={5}
+              gap="gap-[5px]"
               href="/stores"
             />
           )}
