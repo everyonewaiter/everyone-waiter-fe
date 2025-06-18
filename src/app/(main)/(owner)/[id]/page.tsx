e5dc3caf81f87ed04a4fbbc1c4bb6a313dad1486
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-06-18 21:18:58 +0900

    feat: 메인 페이지에 매장 등록 신청 현황 추가

--- src/app/(main)/(owner)/[id]/page.tsx
+++ src/app/(main)/(owner)/[id]/page.tsx
@@ -1,3 +1,11 @@
+import StoreList from "@/app/stores/page";
+import PageTitle from "../../_components/PageTitle";
+
 export default function Page() {
-  return <div>홈페이지</div>;
+  return (
+    <>
+      <PageTitle title="매장 등록 신청 현황" />
+      <StoreList />
+    </>
+  );
 }
