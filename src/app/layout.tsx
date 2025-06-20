1c940b6eae7d16e1cc9216f368a9f3df73e31768
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-06-20 22:10:15 +0900

    refactor: ios와 android icon 추가, 폰트 최적화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,47 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="preload"
+          as="image"
+          href="/logo/logo-with-text.svg"
+          type="image/svg+xml"
+        />
+
+        {/* Apple Touch Icon for iOS */}
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        {/* Favicon (fallback) */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+
+        {/* Manifest icons for Android & PWA */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/icon-512x512.png"
+        />
+
+        {/* Font */}
+        <link
+          rel="preload"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/dynamic-subset-variable.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/dynamic-subset-variable.css"
+          rel="stylesheet"
+        />
+      </head>
       <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-LE0LKNB3BT"
