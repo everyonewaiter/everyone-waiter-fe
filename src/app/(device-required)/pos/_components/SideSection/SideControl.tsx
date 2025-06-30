f978329f0170729a0eaf46ebe1126cab515f960f
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-07-01 00:56:28 +0900

    refactor: 사이드 섹션 버튼 부분 분리

---
+++ src/app/(device-required)/pos/_components/SideSection/SideControl.tsx
@@ -0,0 +1,71 @@
+import Button from "@/components/common/Button/Button";
+import Icon from "@/components/common/Icon";
+import { MinusIcon, PlusIcon } from "lucide-react";
+import { useOrderStore } from "../../_hooks/useOrderStore";
+import useCheckedMenuStore from "../../_hooks/useCheckedMenu";
+
+interface IProps {
+  orderType: DevicePayment;
+  onCancelOrder: () => void;
+  onCancelMenu: () => void;
+}
+
+export default function SideControl({
+  orderType,
+  onCancelOrder,
+  onCancelMenu,
+}: IProps) {
+  const { checkedMenu } = useCheckedMenuStore();
+  const { updateQuantity, orders } = useOrderStore();
+
+  const handleQuantity = (type: "add" | "sub") =>
+    updateQuantity(checkedMenu.menuId, checkedMenu.key, type);
+
+  return orders.length ? (
+    <div className="flex items-center justify-between pb-8">
+      <div className="flex gap-3">
+        {/* quantity - 1 */}
+        <Button
+          variant="outline"
+          color="grey"
+          className="button-xl !w-14 !rounded-[12px] border-gray-600 !px-0 !text-gray-300"
+          onClick={() => handleQuantity("sub")}
+        >
+          <MinusIcon size={24} />
+        </Button>
+        {/* quantity + 1 */}
+        <Button
+          variant="outline"
+          color="grey"
+          className="button-xl !w-14 !rounded-[12px] border-gray-600 !px-0 !text-gray-300"
+          onClick={() => handleQuantity("add")}
+        >
+          <PlusIcon size={24} />
+        </Button>
+      </div>
+    </div>
+  ) : (
+    <div className="flex items-center justify-end gap-3 pb-8">
+      {/* 메뉴 하나 삭제 */}
+      <Button
+        variant="outline"
+        color="grey"
+        className="button-xl !w-14 !rounded-[12px] border-gray-600 !px-0 !text-gray-300"
+        onClick={onCancelMenu}
+      >
+        <MinusIcon size={24} />
+      </Button>
+      {/* 주문 하나 삭제 */}
+      {orders.length === 0 && orderType === "POSTPAID" && (
+        <Button
+          variant="outline"
+          color="grey"
+          className="button-xl !w-14 !rounded-[12px] border-gray-600 !px-0 !text-gray-300"
+          onClick={onCancelOrder}
+        >
+          <Icon iconKey="trash" size={24} />
+        </Button>
+      )}
+    </div>
+  );
+}
