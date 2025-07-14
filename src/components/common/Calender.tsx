df79471aef96db057e583fb5a5611e8a8a86d694
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-07-14 18:44:56 +0900

    fix: 캘린더 디자인 수정

--- src/components/common/Calender.tsx
+++ src/components/common/Calender.tsx
@@ -9,11 +9,11 @@ export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
 type IconProps = LucideProps;
 function IconLeft({ className = "", ...props }: IconProps) {
-  return <ChevronLeft className={cn("h-4 w-4", className)} {...props} />;
+  return <ChevronLeft className={cn("h-6 w-6", className)} {...props} />;
 }
 
 function IconRight({ className = "", ...props }: IconProps) {
-  return <ChevronRight className={cn("h-4 w-4", className)} {...props} />;
+  return <ChevronRight className={cn("h-6 w-6", className)} {...props} />;
 }
 
 function Calendar({
@@ -26,33 +26,33 @@ function Calendar({
     <DayPicker
       showOutsideDays={showOutsideDays}
       className={cn(
-        "w-[340px] bg-white p-5 shadow-xs lg:flex lg:w-[550px] lg:flex-col lg:items-center lg:p-6",
+        "w-[340px] rounded-[24px] bg-white p-5 lg:flex lg:w-[550px] lg:flex-col lg:items-center lg:p-6",
         className
       )}
+      style={{ boxShadow: "0px 2px 20px rgba(0, 0, 0, 0.08)" }}
       formatters={{
         formatWeekdayName: (weekday) =>
           ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"][weekday.getDay()],
       }}
       classNames={{
         // 월 컨테이너
-        months: "flex flex-col lg:flex-row gap-2",
-        month: "flex flex-col gap-6",
+        months: "flex flex-col lg:flex-row gap-2 w-full justify-center",
+        month: "flex flex-col gap-8 w-full",
         // 상단 네비게이션 영역
-        caption: "flex justify-center mb-0 relative items-center w-full",
+        caption: "flex justify-center relative items-center w-full",
         caption_label: "text-xl font-bold text-gray-0",
         nav: "flex items-center gap-1",
         nav_button: "size-7 bg-transparent p-0 opacity-50 hover:opacity-100",
-        nav_button_previous: "absolute left-10 lg:left-10 text-gray-200",
-        nav_button_next: "absolute right-7 lg:right-7 text-gray-200",
+        nav_button_previous: "absolute left-40 text-gray-200",
+        nav_button_next: "absolute right-40 text-gray-200",
         // 달력 테이블
         table: "w-full border-collapse space-x-1",
-        head_row: "flex items-center h-[32px] lg:h-10",
-        head_cell:
-          "text-gray-0 text-s w-[calc(100%/7)] font-normal w-[32px] lg:w-10 ",
-        row: "flex w-full mt-[6px] lg:mt-2 flex items-center",
+        head_row: "w-full flex items-center justify-between h-[32px] lg:h-10",
+        head_cell: "text-gray-0 text-s w-10 text-center font-normal",
+        row: "flex w-full mt-[6px] lg:mt-2 items-center justify-between",
         // 날짜 셀과 날짜
         cell: cn(
-          "w-[calc(100%/7)] relative p-0",
+          "relative p-0",
           // 범위 선택된 셀들을 하나의 배경으로 연결
           "[&:has(.day-range-start)]:bg-primary/[0.06]",
           "[&:has(.day-range-end)]:bg-primary/[0.06]",
@@ -62,14 +62,14 @@ function Calendar({
           "[&:has(.day-range-end)]:rounded-r-full"
         ),
         day: cn(
-          " size-[32px] lg:size-10",
+          "size-[32px] lg:size-10 text-sm",
           "flex items-center justify-center",
           "hover:bg-primary/[0.06]",
           "text-gray-0"
         ),
         // 특수 상태의 날짜
         // 선택된 날짜
-        day_selected: "bg-primary  focus:bg-primary rounded-full",
+        day_selected: "bg-primary focus:bg-primary rounded-full text-white",
         // 오늘 날짜
         day_today: "bg-accent text-accent-foreground",
         // 현재 월에 속하지 않는 날짜
--- src/components/common/DatePicker.tsx
+++ src/components/common/DatePicker.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { format } from "date-fns";
 import { CalendarDays as CalendarIcon } from "lucide-react";
 import dynamic from "next/dynamic";
-import * as React from "react";
+import { useState } from "react";
 import Button from "@/components/common/Button/Button";
 import {
   Popover,
@@ -17,32 +16,39 @@ const Calendar = dynamic(() => import("@/components/common/Calender"), {
 });
 
 export default function DatePicker() {
-  const [date, setDate] = React.useState<Date>();
+  const now = new Date();
+  const [date, setDate] = useState<Date | null>(now);
+  const [open, setOpen] = useState(false);
 
   return (
-    <Popover>
+    <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <Button
           variant="outline"
           color="black"
           className={cn(
-            "relative h-[48px] w-[280px] justify-between border border-gray-500 px-4 py-3 pr-4 text-left text-[16px] font-normal text-gray-500",
+            "relative h-[48px] w-[280px] justify-between rounded-[10px] border border-gray-500 px-4 py-3 pr-4 text-left text-[16px] font-normal",
             !date && "text-muted-foreground"
           )}
         >
           {date ? (
-            format(date, "yyyy.MM.dd")
+            <span className="text-gray-0">
+              {date.toISOString().split("T")[0].split("-").join(".")}
+            </span>
           ) : (
             <span className="text-[16px] text-gray-200">YYYY.MM.DD</span>
           )}
           <CalendarIcon className="absolute right-3 size-6 text-gray-500" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
+      <PopoverContent className="mt-2 ml-15 w-auto p-0">
         <Calendar
           mode="single"
-          selected={date}
-          onSelect={setDate}
+          selected={date!}
+          onSelect={(day) => {
+            setDate(day ?? null);
+            setOpen(false);
+          }}
           initialFocus
         />
       </PopoverContent>
