fc7b2e1bd926cc329fdb5d288571309987d29e21
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-07-27 21:44:03 +0900

    feat: 메인 레이아웃에 wrapper 구현

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,8 +1,21 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, Suspense } from "react";
 import MobileHeader from "@/app/(main)/_components/MobileHeader";
+import Loading from "@/components/Loading";
 import Sidebar from "./_components/Sidebar";
 import AuthGuard from "./_components/AuthGuard";
 import PageTitle from "./_components/PageTitle";
+
+function ContentWrapper({ children }: PropsWithChildren) {
+  return (
+    <div className="flex h-full flex-col">
+      <PageTitle />
+
+      <div className="scrollbar-hide flex-1 overflow-y-auto">
+        <AuthGuard>{children}</AuthGuard>
+      </div>
+    </div>
+  );
+}
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
@@ -12,13 +25,9 @@ export default function Layout({ children }: PropsWithChildren) {
         <MobileHeader />
 
         <main className="relative flex-1 overflow-hidden rounded-[20px] px-5 md:h-full md:overflow-visible md:bg-white md:px-6 md:py-5 lg:overflow-hidden lg:p-8">
-          <div className="flex h-full flex-col">
-            <PageTitle />
-
-            <div className="scrollbar-hide flex-1 overflow-y-auto">
-              <AuthGuard>{children}</AuthGuard>
-            </div>
-          </div>
+          <Suspense fallback={<Loading />}>
+            <ContentWrapper>{children}</ContentWrapper>
+          </Suspense>
         </main>
       </div>
     </div>
