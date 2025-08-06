471ee842b16ca9ab245bc2b32f9d662c69a3db7b
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-08-07 00:57:43 +0900

    refactor: 사이드바 메뉴 변경 시 useTransition 적용

--- src/app/(main)/_components/Sidebar/SidebarMenu.tsx
+++ src/app/(main)/_components/Sidebar/SidebarMenu.tsx
@@ -1,7 +1,9 @@
 import SidebarIcon from "@/components/common/Icon/SidebarIcon";
+import Loading from "@/components/Loading";
 import MENU_ITEMS from "@/constants/sidebarMenus";
 import { getComparePath } from "@/utils/getPathname";
 import { usePathname, useRouter } from "next/navigation";
+import { useTransition } from "react";
 
 interface IProps {
   selectedStoreId: string;
@@ -12,67 +14,70 @@ export default function SidebarMenu({ selectedStoreId, permission }: IProps) {
   const navigate = useRouter();
   const pathname = usePathname();
 
+  const [isPending, startTransition] = useTransition();
+
   const comparePath = getComparePath(pathname, permission);
 
   const menu = MENU_ITEMS[permission];
 
+  const handleMenuClick = (href: string) => {
+    if (href === comparePath) return;
+    const targetPath =
+      permission === "OWNER" ? `/${selectedStoreId}${href}` : href;
+
+    startTransition(() => {
+      if (permission === "OWNER" && href === "/") {
+        navigate.push(`/${selectedStoreId}`);
+      } else {
+        navigate.push(targetPath);
+      }
+    });
+  };
+
+  const handlePrefetchURL = (href: string) => {
+    if (permission === "OWNER") {
+      navigate.prefetch(`/${selectedStoreId}${href}`);
+    } else {
+      navigate.prefetch(href);
+    }
+  };
+
+  const isActive = (href: string) => {
+    if (href === "/") return comparePath === "/";
+    return comparePath === href || comparePath.startsWith(`${href}/`);
+  };
+
   return (
     <div className="relative mt-2">
+      {isPending && <Loading />}
       {menu?.length > 1 && (
         <div className="absolute top-[18px] bottom-[18px] left-[11px] w-[2px] bg-gray-600" />
       )}
       <ul>
-        {menu?.map((item) => {
-          const isActive = () => {
-            if (item.href === "/") return comparePath === "/";
-            return (
-              comparePath === item.href ||
-              comparePath.startsWith(`${item.href}/`)
-            );
-          };
-          return (
-            <li key={item.href}>
-              <button
-                type="button"
-                onClick={() => {
-                  if (item.href === comparePath) return;
-                  const targetPath =
-                    permission === "OWNER"
-                      ? `/${selectedStoreId}${item.href}`
-                      : item.href;
-                  if (permission === "OWNER" && item.href === "/") {
-                    navigate.push(`/${selectedStoreId}`);
-                  } else {
-                    navigate.push(targetPath);
-                  }
-                }}
-                onMouseEnter={() => {
-                  if (permission === "OWNER") {
-                    navigate.prefetch(`/${selectedStoreId}${item.href}`);
-                  } else {
-                    navigate.prefetch(item.href);
-                  }
-                }}
-                className={`flex items-center gap-3 px-2 py-[9px] text-[13px] transition-colors lg:text-[16px] ${
-                  isActive() ? "text-primary" : "text-gray-300"
+        {menu?.map((item) => (
+          <li key={item.href}>
+            <button
+              type="button"
+              onClick={() => handleMenuClick(item.href)}
+              onMouseEnter={() => handlePrefetchURL(item.href)}
+              className={`flex items-center gap-3 px-2 py-[9px] text-[13px] transition-colors lg:text-[16px] ${
+                isActive(item.href) ? "text-primary" : "text-gray-300"
+              }`}
+            >
+              <div
+                className={`z-1 size-2 rounded-full ${
+                  isActive(item.href) ? "bg-primary" : "bg-gray-600"
                 }`}
-              >
-                {/* 빨간 점 (활성 메뉴만) */}
-                <div
-                  className={`z-1 size-2 rounded-full ${
-                    isActive() ? "bg-primary" : "bg-gray-600"
-                  }`}
-                />
-                <SidebarIcon
-                  iconKey={item.icon as string}
-                  className={`size-6 shrink-0 ${isActive() ? "text-primary" : "text-gray-300"}`}
-                  style={{ minWidth: "24px", minHeight: "24px" }}
-                />
-                <span className="font-medium">{item.label}</span>
-              </button>
-            </li>
-          );
-        })}
+              />
+              <SidebarIcon
+                iconKey={item.icon as string}
+                className={`size-6 shrink-0 ${isActive(item.href) ? "text-primary" : "text-gray-300"}`}
+                style={{ minWidth: "24px", minHeight: "24px" }}
+              />
+              <span className="font-medium">{item.label}</span>
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
