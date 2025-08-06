7066e86dd361400d21994d8918e51c65ac5c6da2
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-08-07 00:58:02 +0900

    fix: 매장 추가 스키마 메세지 명확하게 변경

--- src/lib/actions/logout.ts
+++ src/lib/actions/logout.ts
@@ -1,12 +1,12 @@
-// src/lib/serverLogout.ts
-
 "use server";
 
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export async function serverLogout() {
   const cookieStore = await cookies();
   cookieStore.delete("accessToken");
   cookieStore.delete("refreshToken");
-  cookieStore.delete("role");
+  cookieStore.delete("permission");
+  redirect("/login");
 }
