c04af2301ea5b65578abb3fcca28e575f8d4ce15
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-08-07 00:58:51 +0900

    feat: 서버 액션을 사용하기 위해 div -> button 변경

--- src/components/InfoPopup.tsx
+++ src/components/InfoPopup.tsx
@@ -5,6 +5,7 @@ import { useRef } from "react";
 import { useModalCloseTriggers } from "@/hooks/useModalCloseTriggers";
 import { useStoreContext } from "@/providers/storeProvider";
 import useAuthStore from "@/stores/useAuthStore";
+import { serverLogout } from "@/lib/actions/logout";
 import Icon from "./common/Icon/Icon";
 
 const popupList = [
@@ -76,26 +77,17 @@ export default function InfoPopup({ close }: { close: () => void }) {
             </span>
           </div>
         ))}
-      <div
-        role="button"
-        tabIndex={0}
-        className="flex h-9 w-full cursor-pointer items-center gap-2 rounded-[8px] px-3 lg:px-5"
-        aria-label="로그아웃"
-        onClick={() => {
-          close();
-          navigate.push("/logout");
-        }}
-        onKeyDown={(e) => {
-          if (e.key === "Enter" || e.key === " ") {
-            close();
-            navigate.push("/logout");
-          }
-        }}
-      >
-        <span className="text-s font-regular text-gray-300 lg:text-sm">
-          로그아웃
-        </span>
-      </div>
+      <form action={serverLogout}>
+        <button
+          type="submit"
+          className="flex h-9 w-full cursor-pointer items-center gap-2 rounded-[8px] px-3 lg:px-5"
+          aria-label="로그아웃"
+        >
+          <span className="text-s font-regular text-gray-300 lg:text-sm">
+            로그아웃
+          </span>
+        </button>
+      </form>
     </div>
   );
 }
