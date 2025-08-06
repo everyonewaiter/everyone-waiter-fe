61a589fde5d837c0aef9fd8312c864a9ce85e3f5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-08-07 01:04:03 +0900

    fix: 로딩 배경 더 어둡게

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -9,7 +9,7 @@ const Lottie = dynamic(() => import("react-lottie"), {
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 z-1000 bg-black/50">
+    <div className="fixed inset-0 z-1000 bg-black/60">
       <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
         <Lottie
           options={{
