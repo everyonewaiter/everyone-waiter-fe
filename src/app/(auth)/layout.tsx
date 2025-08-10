d163c27856e18b69316a041be480713c0fbfd44c
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-08-11 01:21:08 +0900

    fix: 미들웨어 변경

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,25 @@
-import { PropsWithChildren } from "react";
+"use client";
+
+import { getClientCookie } from "@/lib/cookies/client";
+import { useRouter } from "next/navigation";
+import { PropsWithChildren, useEffect } from "react";
 
 export default function Layout({ children }: PropsWithChildren) {
+  const navigate = useRouter();
+  const token = getClientCookie("accessToken");
+
+  useEffect(() => {
+    if (token) {
+      if (
+        document.referrer &&
+        document.referrer.startsWith(window.location.origin)
+      ) {
+        navigate.back();
+      } else {
+        navigate.replace("/");
+      }
+    }
+  }, [navigate, token]);
+
   return children;
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const publicPaths = [
-  "/waiting/my-turn",
-  "/waiting/result",
-  "/waiting/cancel",
-  "/menu/preview",
-];
-
-const authPaths = ["/login", "/signup"];
-
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
@@ -16,27 +7,25 @@ export function middleware(req: NextRequest) {
   const permission = req.cookies.get("permission")?.value;
 
   const isAdminPath = pathname.startsWith("/admin");
+  const isUserPath = pathname.startsWith("/main");
 
-  // NOTE: 관리자 페이지 접근 제한
-  if (isAdminPath && permission !== "ADMIN") {
-    return NextResponse.rewrite(new URL("/not-found", req.url));
+  // NOTE: userPaths 외 접근 제한
+  if (permission === "USER") {
+    if (!isUserPath || !accessToken) {
+      return NextResponse.rewrite(new URL("/not-found", req.url));
+    }
   }
 
-  // NOTE: 로그인한 상태에서 auth 페이지 접근 금지
-  if (accessToken && authPaths.some((path) => pathname.startsWith(path))) {
-    return NextResponse.redirect(new URL("/", req.url));
+  // NOTE: 관리자 페이지 외 접근 제한
+  if (permission === "ADMIN") {
+    if (!isAdminPath || !accessToken) {
+      return NextResponse.rewrite(new URL("/not-found", req.url));
+    }
   }
 
   // NOTE: 로그인 상태가 아닐 때 main에 접근했을 경우
   if (pathname.startsWith("/main") && !accessToken) {
     return NextResponse.rewrite(new URL("/not-found", req.url));
-  }
-
-  // NOTE: 웨이팅 상태가 아닐 떄 접근했을 경우
-  if (publicPaths.some((path) => pathname.startsWith(path))) {
-    if (accessToken) {
-      return NextResponse.redirect(new URL("/", req.url));
-    }
   }
 
   return NextResponse.next();
