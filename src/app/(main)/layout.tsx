a394f8db4df2c7d28cd8d6c2e40cfba565749168
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-08-14 15:48:01 +0900

    fix: 스크롤 수정

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,8 +1,10 @@
 import dynamic from "next/dynamic";
 import { PropsWithChildren, Suspense } from "react";
+import { redirect } from "next/navigation";
 import Loading from "@/components/Loading";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import Spinner from "@/components/common/Spinner";
+import { getToken } from "@/lib/cookies";
 import MobileHeader from "./_components/MobileLayout/MobileHeader";
 import ContentWrapper from "./(owner)/[id]/_components/ContentWrapper";
 import getQueryClient from "../get-query-client";
@@ -20,20 +22,24 @@ export default async function Layout({
   const queryClient = getQueryClient();
 
   const { id } = await params;
+  const token = await getToken("accessToken");
+  const permission = await getToken("permission");
 
   await queryClient.prefetchQuery({
     queryKey: ["store-list"],
     queryFn: () => getStoreList(),
   });
 
+  if (!token || permission === "USER") redirect("/");
+
   return (
     <div className="flex h-screen w-screen flex-col bg-white md:flex-row md:bg-[#F5F5F5]">
       <HydrationBoundary state={dehydrate(queryClient)}>
         <Sidebar initialStoreId={id} />
       </HydrationBoundary>
-      <div className="flex flex-1 flex-col overflow-hidden md:py-5 md:pr-5 md:pl-0 lg:py-8">
+      <div className="flex flex-1 flex-col overflow-x-hidden md:py-5 md:pr-5 md:pl-0 lg:py-8">
         <MobileHeader />
-        <main className="relative flex-1 overflow-hidden rounded-[20px] px-5 md:h-full md:overflow-visible md:bg-white md:px-6 md:py-5 lg:overflow-hidden lg:p-8">
+        <main className="relative flex-1 overflow-x-hidden overflow-y-auto rounded-[20px] px-5 md:h-auto md:bg-white md:px-6 md:py-5 lg:p-8">
           <Suspense fallback={<Loading />}>
             <div className="flex h-full flex-col">
               <ContentWrapper>{children}</ContentWrapper>
