05ecbed1c49bb87f8abe8128d2942eff4973ce46
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-08-17 16:14:41 +0900

    fix: 메뉴 상세 모달 크기 수정

--- src/app/(main)/(owner)/[id]/@modal/menu/create/layout.tsx
+++ src/app/(main)/(owner)/[id]/@modal/menu/create/layout.tsx
@@ -3,7 +3,7 @@ import ClientRefWrapper from "../_components/Wrapper";
 
 export default async function Layout({ children }: PropsWithChildren) {
   return (
-    <ClientRefWrapper className="md:h-[621px] md:w-[912px] lg:h-[832px] lg:w-[1344px]">
+    <ClientRefWrapper className="aspect-[1344/832] md:h-auto md:min-w-[1000px] lg:h-[832px] lg:w-[1344px]">
       {children}
     </ClientRefWrapper>
   );
