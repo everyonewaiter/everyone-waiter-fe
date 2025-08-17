bae46ed0bee6d2def84a1bd0541d1e04e5f5ea0c
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-08-17 18:49:12 +0900

    style: 불필요한 주석 삭제

--- src/app/(main)/admin/users/layout.tsx
+++ src/app/(main)/admin/users/layout.tsx
@@ -1,15 +1,11 @@
 import { PropsWithChildren } from "react";
 import PageTitle from "../../_components/PageTitle/PageTitle";
 
-export default function Layout({
-  children,
-  // modal,
-}: PropsWithChildren) {
+export default function Layout({ children }: PropsWithChildren) {
   return (
     <div className="h-full">
       <PageTitle initialTitle="회원 관리" />
       {children}
-      {/* {modal} */}
     </div>
   );
 }
