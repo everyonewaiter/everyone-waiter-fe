0720bd5a72c21abc1be85292da51a1a483911921
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-08-19 11:00:05 +0900

    fix: 어드민 페이지 레이아웃 수정

--- src/app/(main)/admin/stores/layout.tsx
+++ src/app/(main)/admin/stores/layout.tsx
@@ -3,10 +3,9 @@ import PageTitle from "../../_components/PageTitle/PageTitle";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <>
+    <div className="flex h-full flex-col">
       <PageTitle initialTitle="매장 등록 승인" />
       {children}
-      {/* {modal} */}
-    </>
+    </div>
   );
 }
--- src/app/(main)/admin/users/layout.tsx
+++ src/app/(main)/admin/users/layout.tsx
@@ -3,7 +3,7 @@ import PageTitle from "../../_components/PageTitle/PageTitle";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <div className="h-full">
+    <div className="flex h-full flex-col">
       <PageTitle initialTitle="회원 관리" />
       {children}
     </div>
