504994e5407c71da5792f18a4cd25503021a3a4d
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-08-26 23:31:18 +0900

    fix: 빌드 에러 수정

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,18 +1,9 @@
 import { PropsWithChildren, ReactNode } from "react";
-import { notFound } from "next/navigation";
 
 export default async function Layout({
   children,
   modal,
-  searchParams,
-}: PropsWithChildren<{
-  modal: ReactNode;
-  searchParams: Promise<{ accessToken: string; storeId: string }>;
-}>) {
-  const { accessToken, storeId } = await searchParams;
-
-  if (!accessToken || !storeId) notFound();
-
+}: PropsWithChildren<{ modal: ReactNode }>) {
   return (
     <>
       {children}
