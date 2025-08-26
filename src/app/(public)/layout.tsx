1b81dd051ed3e3a34826667139045c7e5f0f928d
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-08-26 23:21:40 +0900

    fix: 인증 처리 위치 변경 csr -> ssr

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,19 +1,22 @@
-"use client";
+import { PropsWithChildren, ReactNode } from "react";
+import { notFound } from "next/navigation";
 
-import { PropsWithChildren, ReactNode, Suspense } from "react";
-import Loading from "@/components/Loading";
-import Guard from "./_component/Guard";
-
-export default function Layout({
+export default async function Layout({
   children,
   modal,
-}: PropsWithChildren<{ modal: ReactNode }>) {
+  searchParams,
+}: PropsWithChildren<{
+  modal: ReactNode;
+  searchParams: Promise<{ accessToken: string; storeId: string }>;
+}>) {
+  const { accessToken, storeId } = await searchParams;
+
+  if (!accessToken || !storeId) notFound();
+
   return (
-    <Suspense fallback={<Loading />}>
-      <Guard>
-        {children}
-        {modal}
-      </Guard>
-    </Suspense>
+    <>
+      {children}
+      {modal}
+    </>
   );
 }
