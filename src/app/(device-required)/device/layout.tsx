2175cf36212d12985ac67edb3040a8926f598081
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-08-28 01:37:32 +0900

    style: 기기 생성 페이지 css 수정

--- src/app/(device-required)/device/layout.tsx
+++ src/app/(device-required)/device/layout.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
 
 export default function Layout({ children }: PropsWithChildren) {
-  return <div className="h-dvh w-dvw bg-gray-700">{children}</div>;
+  return <div className="center h-dvh w-dvw bg-gray-700">{children}</div>;
 }
