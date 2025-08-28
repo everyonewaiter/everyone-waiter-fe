ba15e0e69623597df77de5c98699e46e2f5e36e5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-08-28 15:46:09 +0900

    fix: AuthGuard 삭제

--- src/app/(main)/(owner)/[id]/_components/ContentWrapper.tsx
+++ src/app/(main)/(owner)/[id]/_components/ContentWrapper.tsx
@@ -1,11 +1,10 @@
-import AuthGuard from "@/app/(main)/_components/AuthGuard";
 import { HydrationBoundary } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 
 export default function ContentWrapper({ children }: PropsWithChildren) {
   return (
     <HydrationBoundary>
-      <AuthGuard>{children}</AuthGuard>
+      <div className="scrollbar-hide flex-1">{children}</div>
     </HydrationBoundary>
   );
 }
