f2f766e0b39823474fbb1ac3f90252e607ebc699
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-08-28 15:40:38 +0900

    fix: 재접수 시에도 pending modal 출력

--- src/app/(main)/stores/_components/_templates/StoreList.tsx
+++ src/app/(main)/stores/_components/_templates/StoreList.tsx
@@ -65,7 +65,7 @@ export default function StoreList({ storeId }: IProps) {
           />
         </QueryProviders>
       ));
-    } else if (item.status === "APPLY") {
+    } else if (item.status.endsWith("APPLY")) {
       open(() => (
         <QueryProviders>
           <PendingAcceptModal close={close} />
