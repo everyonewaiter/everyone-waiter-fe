5a8aacfc47084e37a2544338bf5dbf2ddf75b062
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-08-31 16:27:18 +0900

    fix: 메뉴 레이아웃 수정

--- src/app/(main)/(owner)/[id]/menu/page.tsx
+++ src/app/(main)/(owner)/[id]/menu/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { useStoreContext } from "@/providers/storeProvider";
+import PageTitle from "@/app/(main)/_components/PageTitle/PageTitle";
+import PAGE_TITLES from "@/constants/pageTitles";
+import Spinner from "@/components/common/Spinner";
 import GuideAddCategory from "./_components/GuideAddCategory";
 import MenuList from "./_components/MenuList";
 import { categoryQueries } from "./_queries/useCategories";
@@ -8,13 +11,23 @@ import { categoryQueries } from "./_queries/useCategories";
 export default function Page() {
   const { storeId } = useStoreContext();
 
-  const { data } = categoryQueries.useCategories(storeId);
+  const { data, isLoading } = categoryQueries.useCategories(storeId);
 
-  return data?.categories?.length! > 0 ? (
-    <MenuList />
-  ) : (
-    <div className="flex flex-1 items-center justify-center">
-      <GuideAddCategory />
+  return (
+    <div className="flex h-full flex-col">
+      {isLoading && <Spinner />}
+      {data?.categories?.length! > 0 ? (
+        <>
+          <PageTitle initialTitle={PAGE_TITLES.OWNER.menu} storeId={storeId} />
+          <div className="relative flex h-full w-full flex-col">
+            <MenuList />
+          </div>
+        </>
+      ) : (
+        <div className="center flex h-full w-full">
+          <GuideAddCategory />
+        </div>
+      )}
     </div>
   );
 }
