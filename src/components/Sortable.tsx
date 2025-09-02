cef76fc17faeb4b84cead0da4a5645fc8e762ee2
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-09-02 23:03:06 +0900

    fix: 카테고리 폼 드래그 수정

--- src/components/Sortable.tsx
+++ src/components/Sortable.tsx
@@ -2,21 +2,22 @@
 
 import {
   DndContext,
-  PointerSensor,
   closestCenter,
   useSensor,
   useSensors,
   SortableContext,
   verticalListSortingStrategy,
 } from "@/components/dnd/index";
+import { Modifiers, MouseSensor, TouchSensor } from "@dnd-kit/core";
 import { Props } from "@dnd-kit/core/dist/components/DragOverlay";
 import { SortingStrategy } from "@dnd-kit/sortable";
 import { NamedExoticComponent, PropsWithChildren } from "react";
 
 interface IProps extends Partial<NamedExoticComponent<Props>> {
-  items: any[];
+  items?: any[];
   onDragEnd: (event: any) => void;
   sortingStrategy?: SortingStrategy;
+  modifiers?: Modifiers;
 }
 
 export default function Sortable({
@@ -24,9 +25,16 @@ export default function Sortable({
   onDragEnd,
   children,
   sortingStrategy,
+  modifiers,
   ...props
 }: PropsWithChildren<IProps>) {
-  const sensors = useSensors(useSensor(PointerSensor));
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: { distance: 5 },
+  });
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: { delay: 150, tolerance: 5 },
+  });
+  const sensors = useSensors(mouseSensor, touchSensor);
 
   const handleDragStart = () => {
     document.body.style.overflow = "hidden";
@@ -46,7 +54,7 @@ export default function Sortable({
       {...props}
     >
       <SortableContext
-        items={items}
+        items={items!}
         strategy={sortingStrategy ?? verticalListSortingStrategy}
         {...props}
       >
