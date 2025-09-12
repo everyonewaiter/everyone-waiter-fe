5e567e4e6b7523d4b9f23c8c43e05afdc8ad856c
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-09-13 01:49:46 +0900

    fix: device 등록 후 페이지 진입 시 해당 디바이스 페이지로 이동

--- src/app/(device-required)/device/layout.tsx
+++ src/app/(device-required)/device/layout.tsx
@@ -1,5 +1,41 @@
-import { PropsWithChildren } from "react";
+"use client";
+
+import Loading from "@/components/Loading";
+import { getDecryptedItem } from "@/lib/auth/secureStorage";
+import { useRouter } from "next/navigation";
+import { PropsWithChildren, useEffect, useState } from "react";
 
 export default function Layout({ children }: PropsWithChildren) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkDeviceInfo = async () => {
+      try {
+        const meta = JSON.parse(localStorage.getItem("@meta") || "{}");
+
+        const deviceInfo = (await getDecryptedItem({
+          key: "@deviceInfo",
+          deviceId: meta.deviceId,
+          storeId: meta.storeId,
+        })) as Device;
+
+        if (deviceInfo.purpose) {
+          router.push(`/${deviceInfo.purpose.toLowerCase()}`);
+        } else {
+          setIsLoading(false);
+        }
+      } catch (error) {
+        setIsLoading(false);
+      }
+    };
+
+    checkDeviceInfo();
+  }, [router]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return <div className="center h-dvh w-dvw bg-gray-700">{children}</div>;
 }
