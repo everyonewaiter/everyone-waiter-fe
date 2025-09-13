81281b2c849bef46707bf83373ebb68e3c863a80
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-09-13 13:17:23 +0900

    feat: device provider에 deviceInfo 추가

--- src/providers/deviceStoreProvider.tsx
+++ src/providers/deviceStoreProvider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { getDecryptedItem } from "@/lib/auth/secureStorage";
 import React, {
   createContext,
   useContext,
@@ -11,6 +12,7 @@ import React, {
 interface DeviceContextType {
   storeId: string | null;
   deviceId: string | null;
+  deviceInfo: Device | null;
 }
 
 const DeviceContext = createContext<DeviceContextType | null>(null);
@@ -26,15 +28,32 @@ export function useDeviceContext() {
 export function DeviceProvider({ children }: { children: React.ReactNode }) {
   const [storeId, setStoreId] = useState<string | null>(null);
   const [deviceId, setDeviceId] = useState<string | null>(null);
+  const [deviceInfo, setDeviceInfo] = useState<Device | null>(null);
 
   useEffect(() => {
     const meta = JSON.parse(localStorage.getItem("@meta") || "{}");
-    setStoreId(meta.storeId ?? null);
-    setDeviceId(meta.deviceId ?? null);
+    const getDeviceInfo = async () => {
+      const d = (await getDecryptedItem({
+        key: "@deviceInfo",
+        deviceId: meta.deviceId,
+        storeId: meta.storeId,
+      })) as Device;
+      return d;
+    };
+
+    (async () => {
+      const d = await getDeviceInfo();
+      setDeviceInfo(d ?? null);
+      setStoreId(meta.storeId ?? null);
+      setDeviceId(meta.deviceId ?? null);
+    })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const value = useMemo(() => ({ storeId, deviceId }), [storeId, deviceId]);
+  const value = useMemo(
+    () => ({ storeId, deviceId, deviceInfo }),
+    [storeId, deviceId, deviceInfo]
+  );
 
   return (
     <DeviceContext.Provider value={value}>{children}</DeviceContext.Provider>
