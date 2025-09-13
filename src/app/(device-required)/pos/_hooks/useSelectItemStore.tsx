cb5685b13e3c4f7b18f7a26a5eefec2fff4bb4e6
chaeyun-sim <111689342+chaeyun-sim@users.noreply.github.com>
2025-09-13 20:23:12 +0900

    fix: QA 메뉴 다중 선택 구현 (#93)
    * feat: 포스에서 메뉴 및 주문 선택 시 다중 선택 구현
    
    * fix: 다중 선택 구현 및 수정
    
    * fix: 조건 수정
    
    * style: console.log 삭제

--- src/app/(device-required)/pos/_hooks/useSelectItemStore.tsx
+++ src/app/(device-required)/pos/_hooks/useSelectItemStore.tsx
@@ -1,22 +1,44 @@
 import { create } from "zustand";
 
+type SelectedMenu = TableOrderMenu & { orderId: string };
+
 interface State {
-  selectedMenu: (TableOrderMenu & { orderId: string }) | null;
-  setSelectedMenu: (
-    value: (TableOrderMenu & { orderId: string }) | null
-  ) => void;
-  updatedMenus: { menuId: string | null; quantity: number | null };
-  updateSelectedMenu: (menuId: string, quantity: number) => void;
-  selectedOrder: TableOrder | null;
-  setSelectedOrder: (value: TableOrder | null) => void;
+  // menu
+  selectedMenu: SelectedMenu[];
+  setSelectedMenu: (value: SelectedMenu[]) => void;
+  addSelectedMenu: (value: SelectedMenu) => void;
+  removeSelectedMenu: (orderMenuId: string) => void;
+  hasSelectedMenu: (orderMenuId: string) => boolean;
+  // order
+  selectedOrder: TableOrder[];
+  setSelectedOrder: (value: TableOrder[]) => void;
+  addSelectedOrder: (value: TableOrder) => void;
+  removeSelectedOrder: (value: string) => void;
+  hasSelectedOrder: (value: string) => boolean;
 }
 
-export const useSelectItemStore = create<State>((set) => ({
-  selectedMenu: null,
+export const useSelectItemStore = create<State>((set, get) => ({
+  selectedMenu: [],
   setSelectedMenu: (value) => set({ selectedMenu: value }),
-  updatedMenus: { menuId: null, quantity: null },
-  updateSelectedMenu: (menuId, quantity) =>
-    set({ updatedMenus: { menuId, quantity } }),
-  selectedOrder: null,
+  removeSelectedMenu: (orderMenuId) =>
+    set((state) => ({
+      selectedMenu: state.selectedMenu.filter(
+        (el) => el.orderMenuId !== orderMenuId
+      ),
+    })),
+  addSelectedMenu: (menu) =>
+    set((state) => ({ selectedMenu: [...state.selectedMenu, menu] })),
+  hasSelectedMenu: (orderMenuId) =>
+    get().selectedMenu.some((el) => el.orderMenuId === orderMenuId),
+
+  selectedOrder: [],
   setSelectedOrder: (value) => set({ selectedOrder: value }),
+  removeSelectedOrder: (orderId) =>
+    set((state) => ({
+      selectedOrder: state.selectedOrder.filter((el) => el.orderId !== orderId),
+    })),
+  addSelectedOrder: (order: TableOrder) =>
+    set((state) => ({ selectedOrder: [...state.selectedOrder, order] })),
+  hasSelectedOrder: (orderId: string) =>
+    get().selectedOrder.some((el) => el.orderId === orderId),
 }));
